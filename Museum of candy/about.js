f7e30cd2b538f82e69f3c4b49cc4e544a688bdcf
chiao <a6218willtreaty@gmail.com>
2025-03-01 18:24:09 +0800

    change name

--- Museum of candy/about.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll',function(){ 
-    const mainNav = document.querySelector('#mainNavbar');
-    if(parseInt(this.documentElement.scrollTop)>parseInt(mainNav.scrollHeight)){
-        mainNav.classList.add('scrolled');
-    }else{
-        mainNav.classList.remove('scrolled');
-    }
-})
